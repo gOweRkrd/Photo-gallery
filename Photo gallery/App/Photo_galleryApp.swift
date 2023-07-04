1c8a8f4ec2f0edd80d606b33dcf648d3d6fd5fa7
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-07-04 12:30:54 +0300

    Add files via upload

---
+++ Photo gallery/App/Photo_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_galleryApp.swift
+//  Photo gallery
+//
+//  Created by Александр Косяков on 04.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Photo_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photo gallery/Resources/R.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+enum StringResources {
+    
+    enum MainView {
+        static let chooseButton = "Выбрать"
+    }
+    
+    enum Colors {
+        static let white = UIColor(hexString: "#FFFFFF")
+        static let black = UIColor(hexString: "#000000")
+        static let lightGray = UIColor(hexString: "#F8F8F8")
+        static let lightBlue = UIColor(hexString: "#00AEFF")
+    }
+}
+
+typealias R = StringResources
