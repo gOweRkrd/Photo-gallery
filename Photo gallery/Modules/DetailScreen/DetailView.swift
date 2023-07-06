cd9a1b5af1db8ed65403f4cfb1c4b6ce78b1ba08
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-07-06 09:34:17 +0300

    Refactoring project

--- Photo gallery/Modules/DetailScreen/DetailView.swift
+++ Photo gallery/Modules/DetailScreen/DetailView.swift
@@ -2,30 +2,45 @@ import SDWebImageSwiftUI
 import SwiftUI
 
 struct DetailView: View {
-    @State private var scale: CGFloat = 1.0
     
     var imageURL: URL?
+    @State private var scale: CGFloat = 1.0
     
     var body: some View {
         ZStack {
             Color(R.Colors.white).edgesIgnoringSafeArea(.all)
-
-            if let imageURL = imageURL {
-                WebImage(url: imageURL)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaleEffect(scale)
-                    .gesture(
-                        MagnificationGesture()
-                            .onChanged { value in
-                                scale = value.magnitude
-                            }
-                    )
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                    .padding(.bottom)
+            
+            if imageURL != nil {
+                imageLoader
             } else {
-                Text("Image not available")
+                errorText
             }
         }
+    }
+}
+
+// MARK: - Ui
+
+private extension DetailView {
+    
+    var imageLoader: some View {
+        WebImage(url: imageURL)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .scaleEffect(scale)
+            .gesture(
+                MagnificationGesture()
+                    .onChanged { value in
+                        scale = value.magnitude
+                    }
+            )
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .padding(.bottom)
+    }
+    
+    var errorText: some View {
+        Text(R.DetailView.errorText)
+            .customFont(SFProDisplay.medium, category: .extraLarge)
+            .foregroundColor(Color(R.Colors.black))
     }
 }
--- Photo gallery/Modules/MainScreen/MainView.swift
+++ Photo gallery/Modules/MainScreen/MainView.swift
@@ -2,47 +2,72 @@ import SDWebImageSwiftUI
 import SwiftUI
 
 struct MainView: View {
+    
     @ObservedObject var viewModel = MainViewModel()
-
+    
     var body: some View {
         ZStack {
             Color(R.Colors.white).edgesIgnoringSafeArea(.all)
             NavigationView {
                 Group {
                     if viewModel.isLoading {
-                        Text("Loading...")
+                        progressView
                     } else if !viewModel.photos.isEmpty {
-                        List(viewModel.photos) { photo in
-                            NavigationLink(destination: DetailView(imageURL: photo.imageURL)) {
-                                VStack {
-                                    if let imageURL = photo.imageURL {
-                                        WebImage(url: imageURL)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                    }
-                                }
-                            }
-                            .buttonStyle(PlainButtonStyle())
-                            .listRowInsets(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
-                        }
-                        .listStyle(.grouped)
+                        mainContent
                     } else {
-                        Text("No photos available")
+                        errorImage
                     }
                 }
-                .navigationTitle("Photo Gallery")
+                .navigationTitle(R.MainView.title)
                 .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("Refresh") {
-                            viewModel.fetchPhotos()
-                        }
+                    ToolbarItem(placement: .navigationBarTrailing) { refreshButton }
+                }
+            }
+            .onAppear { viewModel.fetchPhotos() }
+        }
+    }
+}
+
+// MARK: - Ui
+
+private extension MainView {
+    
+    var progressView: some View {
+        ProgressView().scaleEffect(2.0)
+    }
+    
+    var mainContent: some View {
+        List(viewModel.photos) { photo in
+            NavigationLink(destination: DetailView(imageURL: photo.imageURL)) {
+                VStack {
+                    if let imageURL = photo.imageURL {
+                        WebImage(url: imageURL)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
                     }
                 }
             }
-            .onAppear {
-                viewModel.fetchPhotos()
-            }
+            .buttonStyle(PlainButtonStyle())
+            .listRowInsets(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
         }
+        .listStyle(.grouped)
+    }
+    
+    var errorImage: some View {
+        Image("error404").clipShape(Capsule())
+    }
+    
+    var refreshButton: some View {
+        Button(
+            action: { viewModel.fetchPhotos() },
+            label: { refreshText }
+        )
+    }
+    
+    var refreshText: some View {
+        Text(R.MainView.refreshText)
+            .customFont(SFProDisplay.bold, category: .extraLarge)
+            .foregroundColor(Color(R.Colors.black))
     }
 }
